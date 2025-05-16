c5fd5b76620b696de7c9fb18ea9fca9de9100cc6
Sco-Dot <oliver.scottd@gmail.com>
2025-05-16 16:26:04 -0400

    finish part 1

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+
+const VoteButton = ({ selected, votes, setVotes, max, setMax }) => {
+  const handleClick = () => {
+    const newVotes = [...votes]
+    newVotes[selected]++
+    if (newVotes[selected] > max.val) {
+      const newMax = {index: selected, val: newVotes[selected]}
+      setMax(newMax)
+    }
+    setVotes(newVotes)
+  }
+
+  return (
+    <button onClick={handleClick}>vote</button>
+  )
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+  const [max, setMax] = useState({index: 0, val: 0})
+
+  const handleClick = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length))
+  }
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <div>{anecdotes[selected]}</div>
+      <div>has {votes[selected] ? votes[selected] : 0} votes</div>
+      <VoteButton selected={selected} votes={votes} setVotes={setVotes} max={max} setMax={setMax} />
+      <button onClick={handleClick}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      <div>{anecdotes[max.index]}</div>
+      <div>has {max.val} votes</div>
+    </div>
+  )
+}
+
+export default App
---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+
+const Button = ({ text, onClick }) => (
+  <button onClick={onClick}>{text}</button>
+)
+
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+)
+
+const Statistics = ({ good, neutral, bad }) => {
+  if (good + bad + neutral === 0) {
+    return <div>No feedback given</div>
+  }
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text='good' value={good} />
+        <StatisticLine text='neutral' value={neutral} />
+        <StatisticLine text='bad' value={bad} />
+        <StatisticLine text='all' value={good + neutral + bad} />
+        <StatisticLine text='average' value={(good - bad)/(good + bad + neutral)} />
+        <StatisticLine text='positive' value={(good / (good + bad + neutral) * 100) + ' %'} />
+      </tbody>
+    </table>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <Button text='good' onClick={handleGoodClick} />
+      <Button text='neutral' onClick={handleNeutralClick} />
+      <Button text='bad' onClick={handleBadClick} />
+      <h1>Statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
+}
+
+export default App
